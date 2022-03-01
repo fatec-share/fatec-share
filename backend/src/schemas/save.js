d5055631c74c2a63552137a40dd6aff5c24b28af
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2022-03-01 02:18:12 +0000

    New experimental branch

---
+++ backend/src/schemas/save.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'save',
+    Title: 'Save',
+    type: 'document',
+    fields: [
+        {
+            name: 'postedBy',
+            title: 'Postedby',
+            type: 'postedBy',
+        },
+        {
+            name: 'userId',
+            title: 'UserId',
+            type: 'string',
+        } 
+    ]
+}
