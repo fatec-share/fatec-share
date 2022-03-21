08a5bc8b83714199a4f3ffa0cfca8a393a9c67f9
VeronicaMartins <veronicadmartins@gmail.com>
2022-03-21 00:30:22 -0300

    utilizada a biblioteca axios para fazer a conexão

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -9,22 +9,44 @@ import {
   BotaoEntrar,
   BotaoAbrirConta,
 } from "./styles";
+
+import axios from "axios";
 const Login = () => {
   return (
-    <ContainerLogin>
-      <MargemNomeApp>
-        <NomeAppPrincipal>Fatec</NomeAppPrincipal>
-        <NomeAppSecundario> Share</NomeAppSecundario>
-      </MargemNomeApp>
-      <RetanguloLogin>
-        <TextoRetangulo>Email</TextoRetangulo>
-        <CampoInput></CampoInput>
-        <TextoRetangulo>Senha</TextoRetangulo>
-        <CampoInput></CampoInput>
-        <BotaoEntrar>Entrar</BotaoEntrar>
-        <BotaoAbrirConta>Abrir Conta</BotaoAbrirConta>
-      </RetanguloLogin>
-    </ContainerLogin>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        (async () => {
+          try {
+            const resposta = await axios.post(
+              "http://localhost:3001/usuarios/login",
+              {
+                email: event.target.email.value,
+                senha: event.target.senha.value,
+              }
+            );
+            console.log(`Usuário ${resposta.data.nome} logado com sucesso!`);
+          } catch (error) {
+            console.log("Usuário ou senha incorretos!");
+          }
+        })();
+      }}
+    >
+      <ContainerLogin>
+        <MargemNomeApp>
+          <NomeAppPrincipal>Fatec</NomeAppPrincipal>
+          <NomeAppSecundario> Share</NomeAppSecundario>
+        </MargemNomeApp>
+        <RetanguloLogin>
+          <TextoRetangulo>Email</TextoRetangulo>
+          <CampoInput name="email"></CampoInput>
+          <TextoRetangulo>Senha</TextoRetangulo>
+          <CampoInput name="senha" type="password"></CampoInput>
+          <BotaoEntrar type="submit" value="Entrar" />
+          <BotaoAbrirConta>Abrir Conta</BotaoAbrirConta>
+        </RetanguloLogin>
+      </ContainerLogin>
+    </form>
   );
 };
 export default Login;
