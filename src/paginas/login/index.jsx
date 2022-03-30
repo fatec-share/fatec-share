92a1bf841fffe3d80fcf871dbcd7b057f92b1f4c
VeronicaMartins <veronicadmartins@gmail.com>
2022-03-30 17:26:17 -0300

    Correção de bugs

--- src/paginas/login/index.jsx
+++ src/paginas/login/index.jsx
@@ -1,17 +1,9 @@
-import {
-  LoginContainer,
-  LoginEsquerda,
-  LoginDireita,
-  LoginCartao,
-  LoginCampoTexto
-} from './styles';
+import { LoginContainer, LoginEsquerda, LoginDireita, LoginCartao, LoginCampoTexto } from './styles';
 import { Botao } from '../../componentes/Botao';
 import { validaLogin } from '../../servicos/api';
 import share from '../../assets/home/share.svg';
 
-
 export const Login = () => {
-
   return (
     <form
       onSubmit={(evento) => {
@@ -21,15 +13,18 @@ export const Login = () => {
     >
       <LoginContainer>
         <LoginEsquerda>
-          <h1>Faça login<br />E entre para o Fatec Share</h1>
+          <h1>
+            Faça login
+            <br />E entre para o Fatec Share
+          </h1>
           <img src={share} alt="Share" />
         </LoginEsquerda>
         <LoginDireita>
           <LoginCartao>
             <h1>LOGIN</h1>
             <LoginCampoTexto>
-              <label htmlFor="usuario">Usuário</label>
-              <input type="text" name="usuario" placeholder="Usuário" autoComplete="off" />
+              <label htmlFor="email">Usuário</label>
+              <input type="text" name="email" placeholder="exemplo@fatec.sp.gov.br" autoComplete="off" />
             </LoginCampoTexto>
             <LoginCampoTexto>
               <label htmlFor="senha">Senha</label>
@@ -41,5 +36,5 @@ export const Login = () => {
         </LoginDireita>
       </LoginContainer>
     </form>
-  )
-}
+  );
+};
