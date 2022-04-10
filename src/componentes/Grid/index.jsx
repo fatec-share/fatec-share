9e489fc116a95a129a2b982f71c958c8e386608b
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2022-04-10 15:47:32 -0300

    Update colours schemas

--- src/componentes/Grid/index.jsx
+++ src/componentes/Grid/index.jsx
@@ -5,7 +5,7 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#2f2841',
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: 'center',
@@ -17,17 +17,26 @@ export default function BasicGrid() {
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Grid item xs={8}>
-          <Item>xs=8</Item>
+          <Item>
+            x
+          </Item>
         </Grid>
         <Grid item xs={4}>
-          <Item>xs=4</Item>
+        <Item>
+           x
+          </Item>
         </Grid>
         <Grid item xs={4}>
-          <Item>xs=4</Item>
+        <Item>
+            x
+          </Item>
         </Grid>
         <Grid item xs={8}>
-          <Item>xs=8</Item>
+        <Item>
+            x
+          </Item>
         </Grid>
+        
       </Grid>
     </Box>
   );
